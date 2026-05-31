eab1343653bce3d1f1b2f0b72347c28c35d12593
raja-taha <rajataha0622gmail.com>
2026-05-31 14:38:09 +0500

    initial commit

---
+++ 1 Basics/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "Taha"
+	var password string = "123456"
+
+	fmt.Println("Hello, World!")
+	fmt.Printf("Authentication: %s\n", username+":"+password)
+}
